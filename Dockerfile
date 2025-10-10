8e714afc418c8994778ff60c66824d98fe12632c
Ethan Lee <ethantlee21@gmail.com>
2025-10-10 00:13:13 -0400

    feat: wrap gallery in suspense component
    - Added a suspense component to wrap the gallery component.
    - Removed cache mount from dockerfile to ensure correct dependency installation.

--- Dockerfile
+++ Dockerfile
@@ -9,10 +9,10 @@ COPY . /app
 WORKDIR /app
 
 FROM base AS deps
-RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm install --prod --frozen-lockfile
+RUN pnpm install --prod --frozen-lockfile
 
 FROM base AS build
-RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm install --frozen-lockfile
+RUN pnpm install --frozen-lockfile
 RUN pnpm run build
 
 FROM base AS runner
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Marketplace @ dooleyonline",
@@ -16,7 +16,7 @@ const MarketplaceLayout = ({
   return (
     <>
       {/* Doesn't need fallback since header is hidden until mounted */}
-      {gallery}
+      <Suspense>{gallery}</Suspense>
       {itemDialog}
     </>
   );
